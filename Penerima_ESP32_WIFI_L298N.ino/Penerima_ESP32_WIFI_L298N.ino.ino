bed708b66a0ab1ce66c06468f5fef92830b0d0da
Tegar Subagdja <kingtegar1510@gmail.com>
2025-05-18 17:02:30 +0700

    update control esp32 wifi l298n

--- Penerima_ESP32_WIFI_L298N.ino/Penerima_ESP32_WIFI_L298N.ino.ino
+++ Penerima_ESP32_WIFI_L298N.ino/Penerima_ESP32_WIFI_L298N.ino.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <math.h>
 
 const char* ssid = "ESP32_AP";
 const char* password = "12345678";
@@ -14,8 +15,16 @@ const int IN3 = 25;
 const int IN4 = 33;
 const int ENB = 32;
 
-const int speedA = 200;
-const int speedB = 200;
+const int baseSpeed = 255;
+const uint8_t maxTurnStrength = 128;
+uint8_t turnStrength = 0;
+
+bool forwardPressed = false;
+bool backwardPressed = false;
+bool leftPressed = false;
+bool rightPressed = false;
+
+unsigned long lastUpdateTime = 0;
 
 void setupMotorPins() {
   pinMode(IN1, OUTPUT);
@@ -35,40 +44,47 @@ void stopMotors() {
   ledcWrite(ENB, 0);
 }
 
-void moveForward() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  ledcWrite(ENA, speedA);
-  ledcWrite(ENB, speedB);
+void updateTurnStrength() {
+  if (millis() - lastUpdateTime > 100) {
+    if (leftPressed || rightPressed) {
+      if (turnStrength < maxTurnStrength) turnStrength++;
+    } else {
+      if (turnStrength > 0) turnStrength--;
+    }
+    lastUpdateTime = millis();
+  }
 }
 
-void moveBackward() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  ledcWrite(ENA, speedA);
-  ledcWrite(ENB, speedB);
-}
+void updateMotors() {
+  int leftSpeed = baseSpeed;
+  int rightSpeed = baseSpeed;
+  int reduce = sqrt(turnStrength) * 20;  // Adjust sensitivity here
 
-void turnLeft() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  ledcWrite(ENA, speedA);
-  ledcWrite(ENB, speedB);
-}
+  if (leftPressed && !rightPressed) {
+    leftSpeed = max(baseSpeed - reduce, 0);
+    rightSpeed = baseSpeed;
+  } else if (rightPressed && !leftPressed) {
+    rightSpeed = max(baseSpeed - reduce, 0);
+    leftSpeed = baseSpeed;
+  }
 
-void turnRight() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  ledcWrite(ENA, speedA);
-  ledcWrite(ENB, speedB);
+  if (forwardPressed) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else if (backwardPressed) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  } else {
+    stopMotors();
+    return;
+  }
+
+  ledcWrite(ENA, leftSpeed);
+  ledcWrite(ENB, rightSpeed);
 }
 
 void handleRoot() {
@@ -79,28 +95,60 @@ void handleRoot() {
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>ESP32 Motor Control</title>
       <style>
+        * {
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          -webkit-touch-callout: none;
+        }
+        body {
+          font-family: sans-serif;
+          text-align: center;
+          margin-top: 40px;
+        }
         button {
           width: 100px;
           height: 40px;
           font-size: 16px;
           margin: 5px;
+          touch-action: manipulation;
         }
       </style>
     </head>
     <body>
       <h2>ESP32 Motor Control</h2>
-      <button onclick="sendCmd('forward')">Forward</button><br>
-      <button onclick="sendCmd('left')">Left</button>
-      <button onclick="sendCmd('stop')">Stop</button>
-      <button onclick="sendCmd('right')">Right</button><br>
-      <button onclick="sendCmd('backward')">Backward</button>
+      <div>
+        <button ontouchstart="press('forward')" ontouchend="release('forward')"
+                onmousedown="press('forward')" onmouseup="release('forward')">Forward</button><br>
+
+        <button ontouchstart="press('left')" ontouchend="release('left')"
+                onmousedown="press('left')" onmouseup="release('left')">Left</button>
+
+        <button onclick="stop()">Stop</button>
+
+        <button ontouchstart="press('right')" ontouchend="release('right')"
+                onmousedown="press('right')" onmouseup="release('right')">Right</button><br>
+
+        <button ontouchstart="press('backward')" ontouchend="release('backward')"
+                onmousedown="press('backward')" onmouseup="release('backward')">Backward</button>
+      </div>
 
       <script>
-        function sendCmd(cmd) {
-          fetch('/' + cmd)
-            .then(response => response.text())
-            .then(data => console.log(data));
+        function press(dir) {
+          fetch('/press?dir=' + dir);
         }
+        function release(dir) {
+          fetch('/release?dir=' + dir);
+        }
+        function stop() {
+          fetch('/stop');
+        }
+
+        // Optional: prevent long-press context menu completely
+        document.addEventListener("contextmenu", function(e) {
+          e.preventDefault();
+        }, false);
       </script>
     </body>
     </html>
@@ -110,7 +158,7 @@ void handleRoot() {
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   setupMotorPins();
   stopMotors();
 
@@ -119,23 +167,30 @@ void setup() {
   Serial.println(WiFi.softAPIP());
 
   server.on("/", handleRoot);
-  server.on("/forward", []() {
-    moveForward();
-    server.send(200, "text/plain", "Moving Forward");
+
+  server.on("/press", []() {
+    String dir = server.arg("dir");
+    if (dir == "forward") forwardPressed = true;
+    else if (dir == "backward") backwardPressed = true;
+    else if (dir == "left") leftPressed = true;
+    else if (dir == "right") rightPressed = true;
+    updateMotors();
+    server.send(200, "text/plain", "Pressed " + dir);
   });
-  server.on("/backward", []() {
-    moveBackward();
-    server.send(200, "text/plain", "Moving Backward");
+
+  server.on("/release", []() {
+    String dir = server.arg("dir");
+    if (dir == "forward") forwardPressed = false;
+    else if (dir == "backward") backwardPressed = false;
+    else if (dir == "left") leftPressed = false;
+    else if (dir == "right") rightPressed = false;
+    updateMotors();
+    server.send(200, "text/plain", "Released " + dir);
   });
-  server.on("/left", []() {
-    turnLeft();
-    server.send(200, "text/plain", "Turning Left");
-  });
-  server.on("/right", []() {
-    turnRight();
-    server.send(200, "text/plain", "Turning Right");
-  });
+
   server.on("/stop", []() {
+    forwardPressed = backwardPressed = leftPressed = rightPressed = false;
+    turnStrength = 0;
     stopMotors();
     server.send(200, "text/plain", "Stopped");
   });
@@ -145,5 +200,7 @@ void setup() {
 }
 
 void loop() {
+  updateTurnStrength();
+  updateMotors();
   server.handleClient();
 }
